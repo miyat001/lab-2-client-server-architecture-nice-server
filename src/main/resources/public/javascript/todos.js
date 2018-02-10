76dbf25a84ae28e89a86965107f0e1652fb6c8d2
miyat001 <miyat001@morris.umn.edu>
2018-02-10 14:41:10 -0600

    Add get all todos button
    Added button to return all todos on the page
    Issue #4

---
+++ src/main/resources/public/javascript/todos.js
@@ -0,0 +1,57 @@
+// Why do we use the `var getAllUsers = function()` syntax
+// for the first definition, and the named function syntax
+// for the second definition?
+
+/**
+ * Function to get all the users!
+ */
+function getAllTodos() {
+  console.log("Getting all the todos.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos", function(returned_json){
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+
+function getAllTodosByStatus() {
+  console.log("Getting all the todos.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos?status=" +  document.getElementById("status").value, function(returned_json){
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+
+/**
+ * Wrapper to make generating http requests easier. Should maybe be moved
+ * somewhere else in the future!.
+ *
+ * Based on: http://stackoverflow.com/a/22076667
+ * Now with more comments!
+ */
+function HttpClient() {
+  // We'll take a URL string, and a callback function.
+  this.get = function(aUrl, aCallback){
+    var anHttpRequest = new XMLHttpRequest();
+
+    // Set a callback to be called when the ready state of our request changes.
+    anHttpRequest.onreadystatechange = function(){
+
+      /**
+       * Only call our 'aCallback' function if the ready state is 'DONE' and
+       * the request status is 200 ('OK')
+       *
+       * See https://httpstatuses.com/ for HTTP status codes
+       * See https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+       *  for XMLHttpRequest ready state documentation.
+       *
+       */
+      if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
+        aCallback(anHttpRequest.responseText);
+    };
+
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  }
+}
